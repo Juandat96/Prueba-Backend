220cd7d734615a944f51772800faf3081e9c3df7
Juanda <juandaniel171@gmail.com>
2016-07-18 20:58:50 -0500

    Septimo commit

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,4 +1,7 @@
 class Operation < ActiveRecord::Base
 	belongs_to :worker
 	has_one :inventory
+	scope :ultimos,->{(limit(5).order(:id => :desc))}
+	scope :registro,->(id){Inventory.all("inventory_id = ?", id)}
+	scope :dia,-> {end_of_day("operation_id = ?")}
 end
