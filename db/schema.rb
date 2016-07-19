8d9dc706fc29755d0d3473ffe9435f367746c619
Juanda <juandaniel171@gmail.com>
2016-07-18 19:40:46 -0500

    Tercer commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160719002551) do
+ActiveRecord::Schema.define(version: 20160719003807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,5 +25,12 @@ ActiveRecord::Schema.define(version: 20160719002551) do
   end
 
   add_index "inventories", ["serialnumber"], name: "index_inventories_on_serialnumber", unique: true, using: :btree
+
+  create_table "workers", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
